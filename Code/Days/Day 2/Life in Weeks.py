23ecdeed60754bcceab66a826e6a96af6f7c596b
sankar2019 <33894330+sankar2019@users.noreply.github.com>
2022-07-02 04:26:20 +0530

    Add files via upload

---
+++ Code/Days/Day 2/Life in Weeks.py
@@ -0,0 +1,14 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+remaining_age = 90-int(age)
+rem_months = int(remaining_age * 12)
+rem_weeks = int(remaining_age * 52)
+rem_days = int(remaining_age * 365)
+print(f"You have {rem_days} days, {rem_weeks} weeks, and {rem_months} months left.")
+
+
+
+
